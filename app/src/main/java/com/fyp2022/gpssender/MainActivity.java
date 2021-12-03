2ac4945145c02e786a8e7f7b72cd3af885adc411
Dinkar Kumar Chaubey <dinkar9708@gmail.com>
2021-12-03 17:03:22 +0530

    initial commit

---
+++ app/src/main/java/com/fyp2022/gpssender/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fyp2022.gpssender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
